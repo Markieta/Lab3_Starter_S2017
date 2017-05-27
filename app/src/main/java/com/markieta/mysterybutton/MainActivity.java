b615d6b2f42d7df30d7bcac2adebed42782dc33c
Christopher Markieta <markietachristopher@gmail.com>
2017-05-26 23:36:32 -0400

    Implemented pressMe button click handling

--- app/src/main/java/com/markieta/mysterybutton/MainActivity.java
+++ app/src/main/java/com/markieta/mysterybutton/MainActivity.java
@@ -2,28 +2,51 @@ package com.markieta.mysterybutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    AutoCompleteTextView autoCompleteTextView;
+    LinearLayout linearLayout;
+    LinearLayout.LayoutParams layoutParams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tempView = new TextView(this);
-        tempView.setText("enjoy the rest of the lab!");
-
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.activity_main);
-
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+        autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        linearLayout = (LinearLayout) findViewById(R.id.activity_main);
+        layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT);
 
-        linearLayout.addView(tempView, layoutParams);
-
-        //TODO: change things so that TextViews are added in response to button clicks
         //TODO: populate the AutoCompleteTextView using an ArrayAdapter for String arrays
 
+        Button pressMe = (Button) findViewById(R.id.pressMeButton);
+        pressMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pressMeHandler();
+            }
+        });
+    }
+
+    private void pressMeHandler() {
+        String autoCompleteText = autoCompleteTextView.getText().toString();
+
+        if(autoCompleteText.length() > 0) {
+            TextView tempView = new TextView(this);
+            tempView.setText(autoCompleteText);
+
+            linearLayout.addView(tempView, layoutParams);
+            autoCompleteTextView.getText().clear();
+        }
+        else {
+            autoCompleteTextView.setError(getString(R.string.auto_complete_text_view_error));
+        }
     }
 }
